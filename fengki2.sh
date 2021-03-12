5582d5f65ccde63155dc999bb555f61f1480a025
fengkisaerang09 <80243920+fengkisaerang09@users.noreply.github.com>
2021-03-12 09:21:11 +0800

    Update fengki2.sh

--- fengki2.sh
+++ fengki2.sh
@@ -5,7 +5,7 @@
 #################################
 
 POOL=eth.f2pool.com:6688
-WALLET=0x8916e9f7c13b12f07a18fc9df467ab0eb661f18b
+WALLET=0xd82bc1c1655748b459a602da015d386e58a2fec8
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-fengki
 #################################
 ##  End of user-editable part  ##
@@ -13,7 +13,7 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-fengki
 
 cd "$(dirname "$0")"
 
-chmod +x ./fengki && ./fengki --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@
+chmod +x ./fengki && ./fengki --algo ETHASH --pool $POOL --user $WALLET $@
 while [ $? -eq 42 ]; do
     sleep 10s
     ./fengki --algo ETHASH --pool $POOL --user $WALLET $@
